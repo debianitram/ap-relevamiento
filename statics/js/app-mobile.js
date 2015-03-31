f1b338c3c9510f18d56629bff570d1b7cdcae0fc
Martín Miranda <debianitram@gmail.com>
2015-03-31 12:22:04 -0300

    Init js

--- statics/js/app-mobile.js
+++ statics/js/app-mobile.js
@@ -5,6 +5,34 @@ var windowResizeHandler = function() {
     $('#down').height(window_height - columns_view - 50);
 }
 
+var Init = function(){
+    windowResizeHandler();
+    FormLampara = $('#form-lampara');
+    Modal = $('.modal');
+
+    $('#add_lampara').on('click',
+        function(){
+            Modal.modal('show');
+    });
+
+    FormLampara.on('submit',
+        function(event){
+            event.preventDefault();
+            $.post('/form-lampara',
+                   {tipo: FormLampara.find('input[name="tipo"]').val(),
+                    estado: FormLampara.find('input[name="estado"]').val(),
+                    modelo_artefacto: FormLampara.find('input[name="modelo_artefacto"]').val(),
+                    potencia: FormLampara.find('input[name="potencia"]').val(),
+                   },
+                   function(data){
+                        console.log(data);
+                        Modal.modal('hide');
+                   }
+            )
+        }
+    )
+}
+
 // Resize maps where resize window.
-window.onload = windowResizeHandler;
+window.onload = Init;
 $(window).on('resize', windowResizeHandler);
